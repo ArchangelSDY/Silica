0a0febeca602c63eb4da0ac294714c70f5b685f1
Archangel_SDY <Archangel.SDY@gmail.com>
2014-01-22 00:29:34 +0800

    Fix cache path by making it absolute.

--- db/Database.cpp
+++ db/Database.cpp
@@ -1,9 +1,13 @@
+#include <QCoreApplication>
+
 #include "Database.h"
 
 Database::Database(QObject *parent) :
     QObject(parent)
 {
-    m_cache.setCacheDirectory("netcache");
+    m_cache.setCacheDirectory(
+        QCoreApplication::applicationDirPath() + "/netcache");
     m_net.setCache(&m_cache);
-    connect(&m_net, SIGNAL(finished(QNetworkReply*)), this, SLOT(reqFinish(QNetworkReply*)));
+    connect(&m_net, SIGNAL(finished(QNetworkReply*)),
+        this, SLOT(reqFinish(QNetworkReply*)));
 }
